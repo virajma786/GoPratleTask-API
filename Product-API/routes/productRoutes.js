25ccd2b4aa393248da7c9dc439a4b39156fb450a
vignesh <kiwadevignesh@gmail.com>
2025-08-01 21:02:24 +0530

    all routs set

--- Product-API/routes/productRoutes.js
+++ Product-API/routes/productRoutes.js
@@ -8,4 +8,6 @@ router.get('/', productController.getAllProducts);
     router.get('/:id', productController.getProductById);
     router.post('/', productController.createProduct);
     router.put('/:id', productController.updateProduct);
-router.delete       
+router.delete('/:id', productController.deleteProduct)
+
+module.exports = router;
