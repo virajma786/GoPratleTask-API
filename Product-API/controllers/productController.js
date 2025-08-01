2f630ad0c8d1d43c8c197f30f91c72548e731491
vignesh <kiwadevignesh@gmail.com>
2025-08-01 20:46:32 +0530

    adding search by id fuction

--- Product-API/controllers/productController.js
+++ Product-API/controllers/productController.js
@@ -20,16 +20,25 @@ let nextId = products.length > 0 ? Math.max(...products.map(p => p.id)) + 1 : 1;
 
 
 exports.getAllProducts = (req, res) => { // handiling the the requestes 
-  const { q, page = 1, limit = 10 } = req.query;
-  let filtered = products;
+  
+    const { q, page = 1, limit = 10 } = req.query;
+       let filtered = products;
 
   if (q) {
     filtered = filtered.filter(product =>
-      product.name.toLowerCase().includes(q.toLowerCase())
+        product.name.toLowerCase().includes(q.toLowerCase())
+
     );
   }
 
   const start = (page - 1) * limit;
-  const end = start + +limit;
+    const end = start + +limit;
+
   res.json(filtered.slice(start, end)); // sending the respones as json
+};
+
+exports.getProductById = (req, res) => {  // search by id 
+  const product = products.find(p => p.id === parseInt(req.params.id));
+  if (!product) return res.status(404).send('Product not found');
+  res.json(product);
 };
